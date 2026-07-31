b744cc06905010d920c6dbef87bcc72b08a88531
danielsehr <daniel.serr7028@gmail.com>
2026-08-01 01:29:43 +0200

    fixed: made pw longer

--- src/config/Config.h
+++ src/config/Config.h
@@ -6,7 +6,7 @@
 namespace Config
 {
     constexpr char AP_NAME[] = "Temp_Hum_Dashboard";
-    constexpr char AP_PASSWORD[] = "1234";
+    constexpr char AP_PASSWORD[] = "123456789";
     constexpr uint16_t HHTP_PORT = 80;
     constexpr uint32_t SENSOR_UPDATE_MS = 2000;
 } 
