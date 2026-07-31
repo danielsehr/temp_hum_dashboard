6d782453846d63183d2c4bba3c410c8731541d3d
danielsehr <daniel.serr7028@gmail.com>
2026-08-01 00:11:10 +0200

    add: basic layout for app begin and update

---
+++ src/app/Application.cpp
@@ -0,0 +1,15 @@
+# include "Application.h"
+
+# include <spdlog/spdlog.h>
+
+
+void Application::begin()
+{
+    spdlog::info("Application started!");
+}
+
+
+void Application::update()
+{
+    // Service comes later
+}
