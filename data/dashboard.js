c6515420ee3981abb1ac039399dd5303a313b98a
danielsehr <daniel.serr7028@gmail.com>
2026-08-01 18:28:03 +0200

    changed: to test sensor data coming from websocket

--- data/dashboard.js
+++ data/dashboard.js
@@ -1,8 +1,11 @@
 const status = document.getElementById("status");
+const temperature = document.getElementById("temperature");
+const humidity = document.getElementById("humidity");
 
 const socket = new WebSocket(
     `ws://${location.host}/ws`
 );
+
 
 socket.onopen = () => {
     status.textContent = "Connected";
@@ -12,12 +15,15 @@ socket.onclose = () => {
     status.textContent = "Disconnected";
 };
 
-socket.onmessage = (event) => {
-    console.log(event.data);
-};
 
-document
-    .getElementById("send")
-    .addEventListener("click", () => {
-        socket.send("Hello ESP32");
-    });
+socket.onmessage = (event) => {
+    const message = JSON.parse(event.data);
+
+    if (message.type === "sensor"){
+        temperature.textContent = 
+            `${message.data.temperature.toFixed(1)} °C`;
+
+        humidity.textContent =
+            `${message.data.humidity.toFixed(1)} %`;
+    }
+};
