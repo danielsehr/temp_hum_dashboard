5e73baab62cc4cd1b8d9954207e674b0178df1d5
danielsehr <daniel.serr7028@gmail.com>
2026-08-01 23:46:36 +0200

    add: chart for displaying values from buffer

--- data/dashboard.js
+++ data/dashboard.js
@@ -1,3 +1,31 @@
+const RING_BUFFER_SIZE = 60;
+
+// Chart
+const ctx = 
+    document
+        .getElementById("chart")
+        .getContext("2d");
+
+const chart = new Chart(ctx, {
+    type: "line",
+
+    data: {
+        labels: [],
+        datasets: [{
+            label: "Temperature",
+
+            data: []
+        }]
+    },
+
+    options: {
+        animation: false,
+
+        responsive: true
+    }
+});
+
+// Rest
 const status = document.getElementById("status");
 const temperature = document.getElementById("temperature");
 const humidity = document.getElementById("humidity");
@@ -20,10 +48,29 @@ socket.onmessage = (event) => {
     const message = JSON.parse(event.data);
 
     if (message.type === "sensor"){
+
+        // Live values
         temperature.textContent = 
             `${message.data.temperature.toFixed(1)} °C`;
 
         humidity.textContent =
             `${message.data.humidity.toFixed(1)} %`;
+
+        // Chart
+        chart.data.labels.push(
+            new Date().toLocaleTimeString()
+        );
+
+        chart.data.datasets[0].data.push(
+            message.data.temperature
+        );
+
+        if (chart.data.labels.length > RING_BUFFER_SIZE){
+            chart.data.labels.shift();
+            
+            chart.data.datasets[0].data.shift();
+        }
+
+        chart.update();
     }
 };
