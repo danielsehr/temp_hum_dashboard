f3f4db21d2480f0aac0573f545217126370c3227
danielsehr <daniel.serr7028@gmail.com>
2026-08-01 23:25:29 +0200

    changed interval

--- src/config/Config.h
+++ src/config/Config.h
@@ -8,6 +8,6 @@ namespace Config
     constexpr char AP_NAME[] = "Temp_Hum_Dashboard";
     constexpr char AP_PASSWORD[] = "123456789";
     constexpr uint16_t HTTP_PORT = 80;
-    constexpr uint32_t SENSOR_UPDATE_MS = 2000;
+    constexpr uint32_t SENSOR_UPDATE_MS = 5000;
     constexpr std::size_t RING_BUFFER_SIZE = 60;
 } 
