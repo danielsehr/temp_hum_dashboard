2a6946737e001d75c9271fff9e2316afd7500739
danielsehr <daniel.serr7028@gmail.com>
2026-08-03 14:43:25 +0200

    changed: val for ring buffer size

--- src/config/Config.h
+++ src/config/Config.h
@@ -9,5 +9,5 @@ namespace Config
     constexpr char AP_PASSWORD[] = "123456789";
     constexpr uint16_t HTTP_PORT = 80;
     constexpr uint32_t SENSOR_UPDATE_MS = 5000;
-    constexpr std::size_t RING_BUFFER_SIZE = 60;
+    constexpr std::size_t RING_BUFFER_SIZE = 864;
 } 
