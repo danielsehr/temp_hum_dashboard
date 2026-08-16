8b048e74762f472e277cf3bac8cb966c7ba9c8ba
danielsehr <daniel.serr7028@gmail.com>
2026-08-16 12:12:21 +0200

    mv: History file path to config namespace

--- src/config/Config.h
+++ src/config/Config.h
@@ -8,6 +8,9 @@ namespace Config
     constexpr char AP_NAME[] = "Temp_Hum_Dashboard";
     constexpr char AP_PASSWORD[] = "123456789";
     constexpr uint16_t HTTP_PORT = 80;
+    
     constexpr uint32_t SENSOR_UPDATE_MS = 5000;
     constexpr std::size_t RING_BUFFER_SIZE = 864;
+
+    static constexpr const char* HISTORY_FILE = "/experiments/exp001.csv";
 } 
