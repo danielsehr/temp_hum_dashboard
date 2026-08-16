b69322c41d1c8b4ec741599265aecbe7536494ef
danielsehr <daniel.serr7028@gmail.com>
2026-08-16 11:48:37 +0200

    add: connection status when socket opens or close

--- data/js/dashboard.js
+++ data/js/dashboard.js
@@ -5,14 +5,17 @@ import { WebSocketClient } from "./web/websocket-client.js";
 const socket = new WebSocketClient();
 const currentMeasurements = new CurrentMeasurements();
 const chartManager = new ChartManager();
+const status = document.getElementById("status")
 
 
 socket.onopen(() => {
     status.textContent = "Connected";
+    status.classList.add("connected")
 });
 
 socket.onclose(() => {
     status.textContent = "Disconnected";
+    status.classList.remove("connected")
 });
 
 
