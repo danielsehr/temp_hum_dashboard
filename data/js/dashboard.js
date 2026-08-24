c76fdba01c76c29a7ed1b9a01e2f6df6b0e6493b
danielsehr <daniel.serr7028@gmail.com>
2026-08-25 00:31:56 +0200

    changed: restructuring script by moving functions and vars by dependency to own new classes ExperimentUi and ExperimentManager

--- data/js/dashboard.js
+++ data/js/dashboard.js
@@ -1,20 +1,16 @@
+import { WebSocketClient } from "./web/websocket-client.js";
 import { CurrentMeasurements } from "./ui/current-measurements.js";
 import { ChartManager } from "./ui/chart-manager.js";
-import { WebSocketClient } from "./web/websocket-client.js";
+import { ExperimentUi} from "./ui/experiment-ui.js";
+import { ExperimentManager } from "./experiment/experiment-manager.js";
 
 const socket = new WebSocketClient();
+const currentMeasurements = new CurrentMeasurements();
+const chartManager = new ChartManager();
+const experimentUi = new ExperimentUi();
+const experimentManager = new ExperimentManager(experimentUi);
 
 const connectionStatus = document.getElementById("connectionStatus")
-const experimentStatus = document.getElementById("experimentStatus")
-
-const currentMeasurements = new CurrentMeasurements();
-
-const startButton = document.getElementById("startExperiment");
-const stopButton = document.getElementById("stopExperiment");
-const downloadButton = document.getElementById("downloadCsv");
-
-const chartManager = new ChartManager();
-
 
 
 socket.onopen(() => {
@@ -22,12 +18,12 @@ socket.onopen(() => {
     connectionStatus.classList.add("connected")
 });
 
+
 socket.onclose(() => {
     connectionStatus.textContent = "Disconnected";
     connectionStatus.classList.remove("connected")
 });
 
-initializeExperimentState();
 
 socket.onMeasurement((measurement) => {
     currentMeasurements.updateCurrentMeasurement(measurement);
@@ -35,61 +31,4 @@ socket.onMeasurement((measurement) => {
     chartManager.addMeasurement(measurement);
 });
 
-
-startButton.addEventListener("click", async () => {
-    const response = await fetch("/api/experiment/start", {method: "POST"})
-
-    if (!response.ok) {
-        console.error("Failed to start experiment");
-        return;
-    }
-
-    updateExperimentUi(true);
-})
-
-stopButton.addEventListener("click", async () => {
-    const response = await fetch("/api/experiment/stop", {method: "POST"})
-
-    if (!response.ok) {
-        console.error("Failed to stop experiment");
-        return;
-    }
-
-    updateExperimentUi(false);
-})
-
-function updateExperimentUi(recording) {
-    startButton.disabled = recording;
-    stopButton.disabled = !recording;
-
-    experimentStatus.textContent =
-        recording
-            ? "Recording"
-            : "Not recording";
-}
-
-async function initializeExperimentState() {
-    const response = await fetch("/api/experiment/status");
-
-    if (!response.ok) {
-        console.error(
-            "Failed to retrieve experiment status."
-        );
-        return;
-    }
-
-    const state = await response.json();
-
-    updateExperimentUi(state.recording);
-}
-
-downloadButton.addEventListener("click", () => {
-    const link = document.createElement("a");
-    
-    // window.location.href = "/api/experiment/download";
-    link.href = "/api/experiment/download";
-    
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-});
+experimentManager.initialize()
