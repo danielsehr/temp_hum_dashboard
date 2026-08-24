31693b893217d4b93e9f308035374ee4ca4e7475
danielsehr <daniel.serr7028@gmail.com>
2026-08-24 19:15:51 +0200

    add: start and stop button eventlistener, add: function updateExperimentUi and initializeExperimentState for updating experiment state, add: eventlisteneer for download button

--- data/js/dashboard.js
+++ data/js/dashboard.js
@@ -3,21 +3,31 @@ import { ChartManager } from "./ui/chart-manager.js";
 import { WebSocketClient } from "./web/websocket-client.js";
 
 const socket = new WebSocketClient();
+
+const connectionStatus = document.getElementById("connectionStatus")
+const experimentStatus = document.getElementById("experimentStatus")
+
 const currentMeasurements = new CurrentMeasurements();
+
+const startButton = document.getElementById("startExperiment");
+const stopButton = document.getElementById("stopExperiment");
+const downloadButton = document.getElementById("downloadCsv");
+
 const chartManager = new ChartManager();
-const status = document.getElementById("status")
+
 
 
 socket.onopen(() => {
-    status.textContent = "Connected";
-    status.classList.add("connected")
+    connectionStatus.textContent = "Connected";
+    connectionStatus.classList.add("connected")
 });
 
 socket.onclose(() => {
-    status.textContent = "Disconnected";
-    status.classList.remove("connected")
+    connectionStatus.textContent = "Disconnected";
+    connectionStatus.classList.remove("connected")
 });
 
+initializeExperimentState();
 
 socket.onMeasurement((measurement) => {
     currentMeasurements.updateCurrentMeasurement(measurement);
@@ -26,8 +36,60 @@ socket.onMeasurement((measurement) => {
 });
 
 
-document
-    .getElementById("downloadCsv")
-    .addEventListener("click", () => {
-        window.location.href = "/experiments/exp001.csv";
-    });
+startButton.addEventListener("click", async () => {
+    const response = await fetch("/api/experiment/start", {method: "POST"})
+
+    if (!response.ok) {
+        console.error("Failed to start experiment");
+        return;
+    }
+
+    updateExperimentUi(true);
+})
+
+stopButton.addEventListener("click", async () => {
+    const response = await fetch("/api/experiment/stop", {method: "POST"})
+
+    if (!response.ok) {
+        console.error("Failed to stop experiment");
+        return;
+    }
+
+    updateExperimentUi(false);
+})
+
+function updateExperimentUi(recording) {
+    startButton.disabled = recording;
+    stopButton.disabled = !recording;
+
+    experimentStatus.textContent =
+        recording
+            ? "Recording"
+            : "Not recording";
+}
+
+async function initializeExperimentState() {
+    const response = await fetch("/api/experiment/status");
+
+    if (!response.ok) {
+        console.error(
+            "Failed to retrieve experiment status."
+        );
+        return;
+    }
+
+    const state = await response.json();
+
+    updateExperimentUi(state.recording);
+}
+
+downloadButton.addEventListener("click", () => {
+    const link = document.createElement("a");
+    
+    // window.location.href = "/api/experiment/download";
+    link.href = "/api/experiment/download";
+    
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+});
